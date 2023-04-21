875fec64e5ab889a0d399e43a423d189571524d7
Antony <antonyayansi@gmail.com>
2023-04-21 15:25:41 -0500

    promedios de notas

--- practica05/ejercicio01.cpp
+++ practica05/ejercicio01.cpp
@@ -13,7 +13,7 @@ struct sAlumno{
     float promedio_final;
 };
 
-sAlumno registrarAlumno();
+sAlumno registrarAlumno(int i);
 void mostrarAprobadosDesaprobados(sAlumno alumnos[MAX], int cantAlumnos);
 
 int main(){
@@ -39,21 +39,22 @@ int main(){
 
 	switch(opcion){
 	    case 1:
-		cout << "Ingresar alumnos" << endl;
-		for(int i = 0; i < cantAlumnos; i++){
-		    alumnos[i] = registrarAlumno();
-		}
-		break;
+			cout << "Ingresar alumnos" << endl;
+			for(int i = 0; i < cantAlumnos; i++){
+				alumnos[i] = registrarAlumno(i);
+			}
+			break;
 	    case 2:
-		cout << "================================================" << endl;
-		cout << "Mostrando Nro de Alumnos Aprobado y Desaprobados" << endl;
-		mostrarAprobadosDesaprobados(alumnos, cantAlumnos);
-		break;
+			cout << "================================================" << endl;
+			cout << "Mostrando Nro de Alumnos Aprobado y Desaprobados" << endl;
+			cout << "================================================" << endl;
+			mostrarAprobadosDesaprobados(alumnos, cantAlumnos);
+			break;
 	    case 3:
-		cout << "Salir" << endl;
+			cout << "Salir" << endl;
 		break;
-	    default:
-		cout << "Opcion incorrecta" << endl;
+	    	default:
+			cout << "Opcion incorrecta" << endl;
 		break;
 	}
 
@@ -64,44 +65,62 @@ int main(){
 }
 
 
-sAlumno registrarAlumno(){
+sAlumno registrarAlumno(int i){
 	sAlumno alumno;
 
 	cout << "Ingrese el codigo del alumno: ";
 	cin >> alumno.codigo;
 
-	cout << "Ingrese el nombre del alumno: ";
+	cout << "Ingrese el nombre del alumno [" << alumno.codigo << "]: ";
 	cin >> alumno.nombre;
 
-	cout << "Ingrese el sexo del alumno: ";
-	cin >> alumno.sexo;
+	//validar que el sexo sea M o F
+	do{
+	    cout << "Ingrese el sexo del alumno (F/M) [" << alumno.codigo << "]: ";
+	    cin >> alumno.sexo;
+	}while(alumno.sexo != 'M' && alumno.sexo != 'F');
 
-	cout << "Ingrese el promedio de practicas del alumno: ";
+	cout << "Ingrese el promedio de practicas del alumno [" << alumno.codigo << "]: ";
 	cin >> alumno.promedio_practicas;
 
-	cout << "Ingrese la nota de parciales del alumno: ";
+	cout << "Ingrese la nota de parciales del alumno [" << alumno.codigo << "]: ";
 	cin >> alumno.nota_parciales;
 
-	cout << "Ingrese la nota final del alumno: ";
+	cout << "Ingrese la nota final del alumno [" << alumno.codigo << "]: ";
 	cin >> alumno.nota_final;
+
+	cout << "================================================" << endl;
 
 	alumno.promedio_final = (alumno.promedio_practicas*2 + alumno.nota_parciales*2 + alumno.nota_final*3) / 7;
 
 	return alumno;
 }                   
         
-void mostrarAprobadosDesaprobados(sAlumno alumnos[], int cantAlumnos){
+void mostrarAprobadosDesaprobados(sAlumno alumnos[MAX], int cantAlumnos){
  	int aprobados = 0;
  	int desaprobados = 0;
-  
+	int aprobados_m = 0;
+	int aprobados_f = 0;
+
+	cout << "Nombre\t\tPromedio Final\t\tSexo" << endl;
 	for(int i = 0; i < cantAlumnos; i++){
-	    if(alumnos[i].promedio_final >= 4){
-		aprobados++;
+
+	    cout << alumnos[i].nombre << "\t\t" << alumnos[i].promedio_final << "\t\t\t" << alumnos[i].sexo << endl;
+
+	    if(alumnos[i].promedio_final >= 11){
+			aprobados++;
 	    }else{
-		desaprobados++;
+			desaprobados++;
 	    }
+		if(alumnos[i].sexo == 'M' && alumnos[i].promedio_final >= 11){
+			aprobados_m++;
+		}else if(alumnos[i].sexo == 'F' && alumnos[i].promedio_final >= 11){
+			aprobados_f++;
+		}
 	}
- 
+	cout << "================================================" << endl;
 	cout << "Cantidad de alumnos aprobados: " << aprobados << endl;
-        cout << "Cantidad de alumnos desaprobados: " << desaprobados << endl;
+    cout << "Cantidad de alumnos desaprobados: " << desaprobados << endl;
+	cout << "Cantidad de alumnos aprobados de genero masculino: " << aprobados_m << endl;
+	cout << "Cantidad de alumnos aprobados de genero femenino: " << aprobados_f << endl;
 } 
